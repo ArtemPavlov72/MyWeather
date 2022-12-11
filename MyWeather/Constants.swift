50437961f8c11ea186d0f3a9d76e8046e15bbc9e
ArtemPavlov72 <pavlov.tema@gmail.com>
2022-12-11 12:23:55 +0500

    First commit

---
+++ MyWeather/Constants.swift
@@ -0,0 +1,12 @@
+//
+//  Constants.swift
+//  MyWeather
+//
+//  Created by Артем Павлов on 10.12.2022.
+//
+
+import Foundation
+
+ let apiKEY = "1574ca7511704252b03180557221012"
+
+// http://api.weatherapi.com/v1/current.json?key=1574ca7511704252b03180557221012&q=London
